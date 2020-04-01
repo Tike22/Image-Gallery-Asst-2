73517eaf87f195f84cf72225d59a9fe9a41131ac
Tike22 <38795270+Tike22@users.noreply.github.com>
2020-04-01 19:50:18 -0400

    Final Refinements

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -9,8 +9,21 @@ for(let i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/pic' + i + '.jpg'); /* Creating image tag for html and in the image tag it is creating the source attribute.Then it is creating a name for each image and replacing the index as the number for the image  */
     thumbBar.appendChild(newImage);
-    newImage.onclick = function(e) {
-      displayedImage.src = e.target.src; 
+    newImage.onclick = function(current) {
+      displayedImage.src = current.target.src; // selecting the image pathway currently being clicked and setting the display image pathway to that
     }
 }
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() {
+  const brightness = btn.getAttribute('class');
+  if (brightness === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } 
+  if (brightness === 'light') {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
